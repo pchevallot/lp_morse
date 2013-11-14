149ef618661d008a37ecc45e38d299809b7e7530
Kmaschta <maschtaler.kevin.iris@gmail.com>
2013-11-14 16:39:11 +0100

    Opti code

--- Morse.java
+++ Morse.java
@@ -1,99 +1,112 @@
 package morse;
 import java.util.*;
 
-public class Morse {	
-	    private HashMap<String,String> alphaVersmorse;
-	    private HashMap<String,String> morseVersalpha;
+/**
+ * Classe Morse
+ */
+public class Morse {
+	// Définition des variables
+	private HashMap<String,String> alphaVersmorse;
+	private HashMap<String,String> morseVersalpha;
 
-	    Morse() {
-	        this.alphaVersmorse = new HashMap<String,String>();
-	        this.morseVersalpha = new HashMap<String,String>();
+	// Définition des constantes
+	public static final String WORD_SEPARATOR_MORSE = "   ";
+	public static final String CHAR_SEPARATOR_MORSE = ' ';
+	public static final String WORD_SEPARATOR_ALPHA = ' ';
+	public static final String CHAR_SEPARATOR_ALPHA = '';
 
-	        // ALPHABET - Codes récupérés sur l'article Wikipedia
-	        // http://fr.wikipedia.org/wiki/Morse_(alphabet)
-	        enregistre("a", ".-");
-	        enregistre("b", "-...");
-	        enregistre("c", "-.-.");
-	        enregistre("d", "-..");
-	        enregistre("e", ".");
-	        enregistre("f", "..-.	");
-	        enregistre("g", "--.");
-	        enregistre("h", "....");
-	        enregistre("i", "..");
-	        enregistre("j", ".---");
-	        enregistre("k", "-.-");
-	        enregistre("l", ".-..");
-	        enregistre("m", "--");
-	        enregistre("n", "-.");
-	        enregistre("o", "---");
-	        enregistre("p", ".--.");
-	        enregistre("q", "--.-");
-	        enregistre("r", ".-.");
-	        enregistre("s", "...");
-	        enregistre("t", "-");
-	        enregistre("u", "..-");
-	        enregistre("v", "...-");
-	        enregistre("w", ".--");
-	        enregistre("x", "-..-");
-	        enregistre("y", "-.--");
-	        enregistre("z", "--..");
+	/**
+	 * Constructeur de la classe Morse
+	 */
+	public Morse() {
+		this.alphaVersmorse = new HashMap<String,String>();
+		this.morseVersalpha = new HashMap<String,String>();
 
-	        // CHIFFRES
-	        enregistre("0", "-----");
-	        enregistre("1", ".----");
-	        enregistre("2", "..---");
-	        enregistre("3", "...--");
-	        enregistre("4", "....-");
-	        enregistre("5", ".....");
-	        enregistre("6", "-....");
-	        enregistre("7", "--...");
-	        enregistre("8", "---..");
-	        enregistre("9", "----.");
-	        
-	        // PONCTUATION
-	        enregistre(".", ".-.-.-");
-	        enregistre(",", "--..--");
-	        enregistre("?", "..--..");
-	        enregistre("'",".----.");
-	        enregistre("!", "-.-.--");
-	        enregistre("/", "-..-.");
-	        enregistre("(", "-.--.");
-	        enregistre(")", "-.--.-");
-	        enregistre("&", ".-...");
-	        enregistre(":", "---...");
-	        enregistre(";", "-.-.-.");
-	        enregistre("=", "-...-");
-	        enregistre("+", ".-.-.");
-	        enregistre("-", ".-.-.");
-	        enregistre("_", ".-.-.");
-	        enregistre("\"", ".-..-.");
-	        enregistre("$", "...-..-");
-	        enregistre("@", ".--.-.");
+		// ALPHABET - Codes récupérés sur l'article Wikipedia
+		// http://fr.wikipedia.org/wiki/Morse_(alphabet)
+		enregistre("a", ".-");
+		enregistre("b", "-...");
+		enregistre("c", "-.-.");
+		enregistre("d", "-..");
+		enregistre("e", ".");
+		enregistre("f", "..-.	");
+		enregistre("g", "--.");
+		enregistre("h", "....");
+		enregistre("i", "..");
+		enregistre("j", ".---");
+		enregistre("k", "-.-");
+		enregistre("l", ".-..");
+		enregistre("m", "--");
+		enregistre("n", "-.");
+		enregistre("o", "---");
+		enregistre("p", ".--.");
+		enregistre("q", "--.-");
+		enregistre("r", ".-.");
+		enregistre("s", "...");
+		enregistre("t", "-");
+		enregistre("u", "..-");
+		enregistre("v", "...-");
+		enregistre("w", ".--");
+		enregistre("x", "-..-");
+		enregistre("y", "-.--");
+		enregistre("z", "--..");
+
+		// CHIFFRES
+		enregistre("0", "-----");
+		enregistre("1", ".----");
+		enregistre("2", "..---");
+		enregistre("3", "...--");
+		enregistre("4", "....-");
+		enregistre("5", ".....");
+		enregistre("6", "-....");
+		enregistre("7", "--...");
+		enregistre("8", "---..");
+		enregistre("9", "----.");
+		
+		// PONCTUATION
+		enregistre(".", ".-.-.-");
+		enregistre(",", "--..--");
+		enregistre("?", "..--..");
+		enregistre("'",".----.");
+		enregistre("!", "-.-.--");
+		enregistre("/", "-..-.");
+		enregistre("(", "-.--.");
+		enregistre(")", "-.--.-");
+		enregistre("&", ".-...");
+		enregistre(":", "---...");
+		enregistre(";", "-.-.-.");
+		enregistre("=", "-...-");
+		enregistre("+", ".-.-.");
+		enregistre("-", ".-.-.");
+		enregistre("_", ".-.-.");
+		enregistre("\"", ".-..-.");
+		enregistre("$", "...-..-");
+		enregistre("@", ".--.-.");
 
 
-	    }
+	 }
 
-	    // méthode appelée par le constructeur pour charger les conversions
-	    private void enregistre(String lettre, String point) {
-	        this.alphaVersmorse.put(lettre, point);    // pour conversion alphabet vers morse
-	        this.morseVersalpha.put(point,lettre);     // pour conversion morse vers alphabet
-	    }	     
+	 // méthode appelée par le constructeur pour charger les conversions
+	 private void enregistre(String lettre, String point) {
+		this.alphaVersmorse.put(lettre, point);	 // pour conversion alphabet vers morse
+		this.morseVersalpha.put(point,lettre);	// pour conversion morse vers alphabet
+	 }		
 
-	    // traduit de l'alphabet vers le morse
-	    public String alphaToMorse(String lettre) {
-	        String str = this.alphaVersmorse.get(lettre);
-	        if(str == null)                // if no translation
-	            str = " ";                 // we will return " "
-	        return str;
-	    }
+	 // traduit de l'alphabet vers le morse
+	 public String alphaToMorse(String lettre) {
+		String str = this.alphaVersmorse.get(lettre);
+		if(str == null)					 // if no translation
+				str = " ";					// we will return " "
+		return str;
+	 }
 
-	    // traduit du morse vers l'alphabet
-	    public String morseToAlpha(String lettre) {
-	        String str = this.morseVersalpha.get(lettre);
-	        if(str == null)
-	            str = " ";
-	        return str;
+	 // traduit du morse vers l'alphabet
+	 public String morseToAlpha(String lettre) {
+		String str = this.morseVersalpha.get(lettre);
+		if(str == null)
+				str = " ";
+		return str;
 
-	    }
+	 }
 }
 
